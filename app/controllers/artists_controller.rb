92b5215accf839bcdfb02d07bfd06cbae3a94fe1
Rafael Batista <rafaelb24@yahoo.com>
2019-03-20 23:27:28 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,6 +12,8 @@ class ArtistsController < ApplicationController
   def update
     @artist = Artist.find(params[:id])
     @artist.update(artist_params(:name, :bio))
+    
+    redirect_to artist_path(@artist)
   end
 
   def new
