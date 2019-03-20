37f7d57467403088ebc735fe7bd6a85a19c182a5
Rafael Batista <rafaelb24@yahoo.com>
2019-03-20 23:21:15 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,7 +2,6 @@ class ArtistsController < ApplicationController
   def create
     @artist = Artist.new(artist_params(:name, :bio))
     @artist.save 
-    binding.pry
     redirect_to artist_path(@artist)
   end
 
