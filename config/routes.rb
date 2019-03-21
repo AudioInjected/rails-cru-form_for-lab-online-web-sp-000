ea1792f149471e209088b0ec2f2124ecd20faaa5
Rafael Batista <rafaelb24@yahoo.com>
2019-03-21 00:16:19 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 resources :artists, only: [:create, :update, :show, :new, :edit]
 resources :genres, only: [:create, :update, :show, :new, :edit]
-resources :songs, only: [:create, :update, :show, :new, :edit]
+resources :songs, only: [:create, :update, :show, :new, :edit, :index]
 
   
 
