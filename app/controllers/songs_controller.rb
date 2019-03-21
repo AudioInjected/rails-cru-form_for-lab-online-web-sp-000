9a3d4073774bafcc93e192582a1e1b86fcd81cbd
Rafael Batista <rafaelb24@yahoo.com>
2019-03-21 00:02:01 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,13 @@
 class SongsController < ApplicationController
   def create
     @song = Song.new(song_params(:name, :genre_id, :artist_id))
+    @song.save 
+    
+    redirect_to song_path(@song)
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def update
@@ -15,4 +19,10 @@ class SongsController < ApplicationController
 
   def edit
   end
+  
+  private 
+  
+  def song_params(*args) 
+    params.require(:songs).permit(*args)
+  end 
 end
